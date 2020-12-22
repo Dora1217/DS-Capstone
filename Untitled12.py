9ae1bfcd431b8e8e97d0d11170d47ad99506a842
Dora1217 <66267721+Dora1217@users.noreply.github.com>
2020-12-21 17:14:42 -0800

    assignment1

--- Untitled12.py
+++ Untitled12.py
@@ -1,26 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 This notebook will be mainly used for the capstone project.
-
-
-# In[ ]:
-
-
 import pandas as pd
-
-
-# In[ ]:
-
-
 import Numpy as np
-
-
-# In[ ]:
-
-
 print("Hello Capstone Project Course!")
 
